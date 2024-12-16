1e59f26ae30401c55c6ff8cab7c3d4e6dc57ba2c
Nia Skelton <61608491+fka-purpose@users.noreply.github.com>
2024-12-15 18:37:41 -0600

    Update testCase3.js

--- testCase3.js
+++ testCase3.js
@@ -11,4 +11,30 @@
    await page.getByRole('heading', { name: 'In Progress (1)' }).click();
   await page.getByRole('heading', { name: 'Design system updates' }).click();
   await page.getByText('Design', { exact: true }).click();
+
+  const header = await page.locator('header');
+  await expect(header).toContainText('Web Application');
+ // Locate the "In Progress" column
+const inProgressColumn = page.locator('//*[@id="root"]/div/div[2]/main/div/div/div[2]/div');
+
+// Check if the "In Progress" column contains the task "Design system updates"
+const taskElement = inProgressColumn.locator('xpath=//h3[text()="Design system updates"]'); 
+const taskText = await taskElement.textContent();
+await expect(taskElement).toHaveText('Design system updates');
+if (taskText === 'Design system updates') {
+  console.log('Pass: Task "Design system updates" found in the "In Progress" column.');
+} else {
+  console.error('Fail: Task "Design system updates" not found in the "In Progress" column.');
+}
+
+// Locate and check if the "bug" tag exists in the "In Progress" column
+const class1 = inProgressColumn.locator('xpath=//span[text()="Design"]');
+const class1Text = await class1.textContent();
+if (class1Text === 'Design') {
+  console.log('Pass: Tag "Design" found in the "In Progress" column.');
+} else {
+  console.error('Fail: Tag "Design" not found in the "In Progress" column.');
+}
+
+
  });
