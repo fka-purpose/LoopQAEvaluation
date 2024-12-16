e51dcbf0caf91f3b4f4c212a1d8536163a04bc91
Nia Skelton <61608491+fka-purpose@users.noreply.github.com>
2024-12-15 20:06:38 -0600

    Update testCase3.js

--- testCase3.js
+++ testCase3.js
@@ -1,16 +1,9 @@
- //import { test, expect } from '@playwright/test';
+//import { test, expect } from '@playwright/test';
  const { test, expect } = require('@playwright/test');
 
  test('test', async ({ page }) => {
    await page.goto('https://animated-gingersnap-8cf7f2.netlify.app/');
-   await page.getByLabel('Username').click();
-   await page.getByLabel('Username').fill('admin');
-   await page.getByLabel('Password').click();
-   await page.getByLabel('Password').fill('password123');
-   await page.getByRole('button', { name: 'Sign in' }).click();
-   await page.getByRole('heading', { name: 'In Progress (1)' }).click();
-  await page.getByRole('heading', { name: 'Design system updates' }).click();
-  await page.getByText('Design', { exact: true }).click();
+  
 
   const header = await page.locator('header');
   await expect(header).toContainText('Web Application');
